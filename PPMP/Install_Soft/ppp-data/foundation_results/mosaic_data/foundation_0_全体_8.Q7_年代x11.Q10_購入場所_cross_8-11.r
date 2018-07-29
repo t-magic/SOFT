2d02d4ca0d0546280a9c51d58bf8135fb64cb921
t-magic <t-magic@users.noreply.github.com>
2018-07-29 14:34:44 +0900

    2018-07-29

--- PPMP/Install_Soft/ppp-data/foundation_results/mosaic_data/foundation_0_全体_8.Q7_年代x11.Q10_購入場所_cross_8-11.r
+++
@@ -1,6 +0,0 @@
-library(RSVGTipsDevice)
-    csv_mosaic <- read.csv("G:/SVN/Install_Soft/ppp-data/foundation_results/mosaic_data/foundation_0_‘S‘Ì_8.Q7_”N‘ãx11.Q10_w“üêŠ_cross_8-11.csv", row.names=1, check.names=F, fileEncoding="cp932")
-    devSVGTips(file = "G:/SVN/Install_Soft/ppp-data/foundation_results/mosaic_data/foundation_0_‘S‘Ì_8.Q7_”N‘ãx11.Q10_w“üêŠ_mosaic_plot_8-11_sjis.svg", width=10, height=10, bg = "white", fg = "black", onefile=TRUE, toolTipMode = 0, xmlHeader=TRUE)
-    mosaicplot(as.matrix(csv_mosaic), main="Q7_”N‘ã x Q10_w“üêŠ (‘”: 10859)", xlab = "Q10_w“üêŠ", ylab = "Q7_”N‘ã", shade=FALSE, cex.axis=0.66, dir=c("h", "v"), las = 2, color=c("#FFBFCF", "#FFFFAA", "#FFCC99", "#CCFFCC", "#BFF1FF", "#CCCCFF"))
-    dev.off()
-    
--- PPMP/Install_Soft/ppp-data/foundation_results/progs/morph_seq.pl
+++
@@ -1,7 +0,0 @@
-:- encoding(utf8).
-% :- dynamic rule/2.
-% :- multifile rule/2.
-
-rule(concat, ['じ', 'ぶん']).
-rule(concat, ['粉', 'っぽい']).
-rule(concat, ['合う', 'てる']).
